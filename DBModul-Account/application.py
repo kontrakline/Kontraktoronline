9822e18bac9ef44caf6f603ffa6a4a741c2b6e67
kontrakline <kotrakline@gmail.com>
2018-03-15 11:51:11 +0700

    change response

--- DBModul-Account/application.py
+++ DBModul-Account/application.py
@@ -1,21 +1,21 @@
 import logging
 from Route import RAccount
+from Holder import HResponse
 
 def lambda_handler(pParam):
 
     function = pParam["function"]
-    data     = pParam["data"]
+    param    = pParam["data"]
+    response = HResponse()
 
     try :
         command = getattr(RAccount(), function)
         response = command()
-
-        print(response.getData())
     except Exception as e :
         print(e)
 
-    return True
+    return response
 
 
 if __name__ == "__main__" :
-    print(lambda_handler({"function" : "deleteAccount", "data" : []}))
+    print(lambda_handler({"function" : "insertAccount", "data" : []}))
